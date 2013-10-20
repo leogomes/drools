59f0e7230271df232171f73f8e3aba9c911f381b
Leonardo Freitas Gomes <leonardo.f.gomes@gmail.com>
2013-10-20 18:20:22 +0200

    Improving OTNCompilerTest to cover Hashed and Beta nodes

--- drools-compiler/src/test/java/org/drools/compiler/integrationtests/asm/OTNCompilerTest.java
+++ drools-compiler/src/test/java/org/drools/compiler/integrationtests/asm/OTNCompilerTest.java
@@ -13,7 +13,6 @@ import org.drools.core.reteoo.ObjectTypeNode;
 import org.drools.core.reteoo.compiled.CompiledNetwork;
 import org.junit.Test;
 import org.kie.api.KieBase;
-import org.kie.api.KieBaseConfiguration;
 import org.kie.api.io.ResourceType;
 import org.kie.internal.KnowledgeBase;
 import org.kie.internal.KnowledgeBaseFactory;
@@ -25,44 +24,112 @@ import org.kie.internal.io.ResourceFactory;
 public class OTNCompilerTest extends CommonTestMethodBase {
 
 	@Test
-	public void test() {
-		
-        String drl = "";
-        drl += "package org.drools.compiler.test\n";
-        drl += "import org.drools.compiler.Person\n";
-        drl += "rule test1\n";
-        drl += "when\n";
-        drl += "   Person(name == \"Leo\", age == 32)\n";
-        drl += "   Person(name == \"Leo\", happy == true)\n";
-        drl += "then\n";
-        drl += "end\n";
-		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        KieBase kbase = loadKieBase( kbuilder, phreak, drl );
-        
+	public void nonHashedAlphaNodes() {
 
-        ObjectTypeNode otn = getObjectTypeNode( kbase, Person.class );
-        
-        CompiledNetwork compiledNetwork = ObjectTypeNodeCompiler.compile( ((KnowledgeBuilderImpl)kbuilder).getPackageBuilder(), otn ); 
-        
-        assertNotNull( compiledNetwork );
+		String drl = "";
+		drl += "package org.drools.compiler.test\n";
+		drl += "import org.drools.compiler.Person\n";
+		drl += "rule test1\n";
+		drl += "when\n";
+		drl += "   Person(name == \"Leo\", age == 32)\n";
+		drl += "   Person(name == \"Leo\", happy == true, likes == 'mustard')\n";
+		drl += "then\n";
+		drl += "end\n";
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+				.newKnowledgeBuilder();
+		KieBase kbase = loadKieBase(kbuilder, phreak, drl);
 
+		ObjectTypeNode otn = getObjectTypeNode(kbase, Person.class);
+
+		CompiledNetwork compiledNetwork = ObjectTypeNodeCompiler.compile(
+				((KnowledgeBuilderImpl) kbuilder).getPackageBuilder(), otn);
+
+		assertNotNull(compiledNetwork);
 
 	}
-	
-    private ObjectTypeNode getObjectTypeNode(KieBase kbase, Class<?> nodeClass) {
-        List<ObjectTypeNode> nodes = ((InternalRuleBase)((KnowledgeBaseImpl)kbase).ruleBase).getRete().getObjectTypeNodes();
-        for ( ObjectTypeNode n : nodes ) {
-            if ( ((ClassObjectType)n.getObjectType()).getClassType() == nodeClass ) {
-                return n;
-            }
-        }
-        return null;
-    }
 
-    
-	@SuppressWarnings("deprecation")
-	protected KieBase loadKieBase(KnowledgeBuilder kbuilder, RuleEngineOption phreak, String... drlContentStrings) {
+	@Test
+	public void hashedAlphaNodes() {
+
+		String drl = "";
+		drl += "package org.drools.compiler.test\n";
+		drl += "import org.drools.compiler.Person\n";
+		drl += "rule test1\n";
+		drl += "when\n";
+		drl += "   Person(name == 'Leo')\n";
+		drl += "then\n";
+		drl += "end\n";
+		drl += "rule test2\n";
+		drl += "when\n";
+		drl += "   Person(name == 'Ana')\n";
+		drl += "then\n";
+		drl += "end\n";
+		drl += "rule test3\n";
+		drl += "when\n";
+		drl += "   Person(name == 'Jose')\n";
+		drl += "then\n";
+		drl += "end\n";
+		drl += "rule test4\n";
+		drl += "when\n";
+		drl += "   Person(name == 'Maria')\n";
+		drl += "then\n";
+		drl += "end\n";
+
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+				.newKnowledgeBuilder();
+		KieBase kbase = loadKieBase(kbuilder, phreak, drl);
+
+		ObjectTypeNode otn = getObjectTypeNode(kbase, Person.class);
+
+		CompiledNetwork compiledNetwork = ObjectTypeNodeCompiler.compile(
+				((KnowledgeBuilderImpl) kbuilder).getPackageBuilder(), otn);
+
+		assertNotNull(compiledNetwork);
+
+	}
+
+	@Test
+	public void betaNodes() {
+
+		String drl = "";
+		drl += "package org.drools.compiler.test\n";
+		drl += "import org.drools.compiler.Person\n";
+		drl += "rule test1\n";
+		drl += "when\n";
+		drl += "   $p1: Person(name == 'Leo')\n";
+		drl += "   $p2: Person(name == $p1.name)\n";
+		drl += "then\n";
+		drl += "end\n";
+
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+				.newKnowledgeBuilder();
+		KieBase kbase = loadKieBase(kbuilder, phreak, drl);
+
+		ObjectTypeNode otn = getObjectTypeNode(kbase, Person.class);
+
+		CompiledNetwork compiledNetwork = ObjectTypeNodeCompiler.compile(
+				((KnowledgeBuilderImpl) kbuilder).getPackageBuilder(), otn);
+
+		assertNotNull(compiledNetwork);
+
+	}
+
+	private ObjectTypeNode getObjectTypeNode(KieBase kbase, Class<?> nodeClass) {
+		List<ObjectTypeNode> nodes = ((InternalRuleBase) ((KnowledgeBaseImpl) kbase).ruleBase)
+				.getRete().getObjectTypeNodes();
 		
+		for (ObjectTypeNode n : nodes) {
+			if (((ClassObjectType) n.getObjectType()).getClassType() == nodeClass) {
+				return n;
+			}
+		}
+		return null;
+	}
+
+	@SuppressWarnings("deprecation")
+	protected KieBase loadKieBase(KnowledgeBuilder kbuilder,
+			RuleEngineOption phreak, String... drlContentStrings) {
+
 		for (String drlContentString : drlContentStrings) {
 			kbuilder.add(ResourceFactory.newByteArrayResource(drlContentString
 					.getBytes()), ResourceType.DRL);
@@ -71,13 +138,13 @@ public class OTNCompilerTest extends CommonTestMethodBase {
 		if (kbuilder.hasErrors()) {
 			fail(kbuilder.getErrors().toString());
 		}
-//		KieBaseConfiguration kBaseConfig = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
-//	
-//		kBaseConfig.setOption(phreak);
+		// KieBaseConfiguration kBaseConfig =
+		// KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
+		//
+		// kBaseConfig.setOption(phreak);
 		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
 		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
 		return (KieBase) kbase;
 	}
-
 
 }
